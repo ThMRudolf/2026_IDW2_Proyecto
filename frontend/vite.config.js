22bc253cd7c8fbb3e4e102681bfe66e2c96d361e
ThMRudolf <th.m.rudolf@gmail.com>
2026-05-19 20:52:35 -0600

    changes in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __API_URL__: JSON.stringify(process.env.VITE_API_URL || 'http://localhost:8000'),
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      "fifa-2026-frontend.onrender.com",
+      "localhost",
+      "127.0.0.1"
+    ]
   }
 });
